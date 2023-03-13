c3c1c4704b9632c9fd6eb85594ddd101182a1e3a
thais <thaisster@gmail.com>
2023-03-13 17:22:24 -0300

    Operadores

---
+++ operadores/src/operadores/Principal.java
@@ -0,0 +1,22 @@
+package operadores;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int valor;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite um valor: ");
+		valor = teclado.nextInt();
+		
+		System.out.println(valor/2);
+		System.out.println(valor%2);
+		
+		
+	}
+
+}
